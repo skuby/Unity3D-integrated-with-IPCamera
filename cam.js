ede5c5bb642a7fbcadbc79ee17ef8e47adc19d28
sodiq odofin <reigningboss@yahoo.com>
2019-11-15 15:25:45 +0800

    Add files via upload

---
+++ cam.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+// http://docs.unity3d.com/Documentation/ScriptReference/WWW.LoadImageIntoTexture.html
+
+var url = "rtsp://192.168.1.10/media/video1";
+
+function Start () {
+    // Create a texture in DXT1 format
+
+    // NOTE: you may want to experiment with different texture formats, specially in a web context
+    // https://docs.unity3d.com/Documentation/ScriptReference/TextureFormat.html
+    renderer.material.mainTexture = new Texture2D(4, 4, TextureFormat.DXT1, false); 
+    Debug.Log(address);
+    Debug.Log(url);
+    while(true) {
+        // Start a download of the given URL
+        var www = new WWW(url);
+        // wait until the download is done
+        yield www;
+        // assign the downloaded image to the main texture of the object
+        www.LoadImageIntoTexture(renderer.material.mainTexture);
+    }
+}
---
+++ camera.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour {
+	
+	public string uri = "rtsp://192.168.1.10/media/video1";
+	public string username;
+	public string password;
+	
+	
+	Texture2D cam;
+	
+	
+	public void Start() {
+		cam=new Texture2D(5, 1, TextureFormat.RGB24, false);
+		StartCoroutine(Fetch());
+	}
+	
+	
+	public IEnumerator Fetch() {
+		while(true) {
+			Debug.Log("fetching... "+Time.realtimeSinceStartup);
+			
+			WWWForm form = new WWWForm();
+			form.AddField("dummy", "field");    // required by WWWForm
+			WWW www = new WWW(uri, form.data, new System.Collections.Generic.Dictionary<string,string>() {  // using www.headers is depreciated by some odd reason
+				{"Authorization", "Basic " + System.Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(username+":"+password))}
+			});
+			yield return www;
+			
+			if(!string.IsNullOrEmpty(www.error))
+				throw new UnityException(www.error);
+			
+			www.LoadImageIntoTexture(cam);
+		}
+	}
+	
+	
+	public void OnGUI() {
+		GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height), cam);
+	}
+	
+}
